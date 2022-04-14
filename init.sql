1fd3a3200c380d03ce1ad204a1f653a78c90a6c6
zxcvbnm0091 <91945841+zxcvbnm0091@users.noreply.github.com>
2022-04-14 22:54:41 +0700

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,8 +1,10 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR (255),
     username VARCHAR(100) UNIQUE NOT NULL,
-    name VARCHAR(255) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
 );
 
