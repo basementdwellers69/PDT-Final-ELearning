aedb7148125ccd9cbf293efe1c0ce3d02742e9ea
zxcvbnm0091 <91945841+zxcvbnm0091@users.noreply.github.com>
2022-04-15 13:28:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,35 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """ function to show and process login page """
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        conn = db_connection()
+        cur = conn.cursor()
+        sql = """
+            SELECT id, email
+            FROM users
+            WHERE email = '%s' AND password = '%s'
+        """ % (email, password)
+        cur.execute(sql)
+        user = cur.fetchone()
+
+        error = ''
+        if user is None:
+            error = 'Wrong credentials. No user found'
+        else:
+            session.clear()
+            session['user_id'] = user[0]
+            session['username'] = user[1]
+            return redirect(url_for('index'))
+
+        flash(error)
+        cur.close()
+        conn.close()
+
+    return render_template('login.html')
