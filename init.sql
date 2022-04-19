74722e892aba1dc17266442392adf61fc4bf93d4
GeraldSutarso <92673939+GeraldSutarso@users.noreply.github.com>
2022-04-20 00:36:30 +0700

    Update init.sql
    status untuk pembeda dosen/student(?), valuesnya masih kosong

--- init.sql
+++ init.sql
@@ -1,12 +1,20 @@
+DROP TABLE IF EXISTS major;
+CREATE TABLE major (
+    id SERIAL PRIMARY KEY,
+    major_name VARCHAR (255) NOT NULL
+);
+
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
+    status CHAR(20) NOT NULL UNIQUE KEY,
     firstName VARCHAR(255) NOT NULL,
     lastName VARCHAR (255),
     username VARCHAR(100) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     major_id INT,
+    status VARCHAR(255) NOT NULL,
     CONSTRAINT fk_major FOREIGN KEY (major_id) REFERENCES major(id)
 );
 
@@ -19,10 +27,7 @@ CREATE TABLE articles (
     CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-DROP TABLE IF EXISTS major;
-CREATE TABLE major (
-    id SERIAL PRIMARY KEY,
-    major_name VARCHAR (255) NOT NULL
-);
+INSERT INTO users (id, status, firstName, lastName, username, email, password, major_id)
+VALUES ('');
 
-INSERT INTO major (major_name\) VALUES ('Information Technology'), ('Information System'), ('Visual Communication Design');
+INSERT INTO major (major_name) VALUES ('Information Technology'), ('Information System'), ('Visual Communication Design');
