5770c5e74bd0009e6cd2d04043ac02d25b4c7d08
zxcvbnm0091 <91945841+zxcvbnm0091@users.noreply.github.com>
2022-04-20 16:40:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
-from flask import Flask, request, render_template, session, flash, redirect, url_for, jsonify
+from flask import Flask, redirect, jsonify, render_template, url_for, session, request, flash
 import re
 import psycopg2.extras
 from werkzeug.security import generate_password_hash, check_password_hash
 
-
 from db import db_connection
 
 app = Flask(__name__)
+app.secret_key = 'THISISMYSECRETKEY'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    conn = db_connection()
+    cur = conn.cursor()
+    sql = """
+        SELECT art.id, art.title, art.body, art.user_id
+        FROM articles art
+        ORDER BY art.title
+    """
+    cur.execute(sql)
+    articles = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template('index.html', articles=articles)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -28,6 +39,7 @@ def login():
         """ % (email, password)
         cur.execute(sql)
         user = cur.fetchone()
+        print(user)
 
         error = ''
         if user is None:
@@ -44,10 +56,48 @@ def login():
 
     return render_template('login.html')
 
+
 @app.route('/logout')
 def logout():
     """ function to do logout """
     session.clear()  # clear all sessions
     return redirect(url_for('index'))
 
+@app.route('/article/create', methods=['GET', 'POST'])
+def create():
+    # check if user is logged in
+    if not session:
+        return redirect(url_for('login'))
 
+    if request.method == 'POST':
+        data = request.get_json() or {}
+        # check existence of title and body
+        if data.get('title') and data.get('body'):
+            title = data.get('title', '')
+            body = data.get('body', '')
+            user_id = session.get('user_id')
+
+            # strip() is to remove excessive whitespaces before saving
+            title = title.strip()
+            body = body.strip()
+
+            conn = db_connection()
+            cur = conn.cursor()
+            # insert with the user_id
+            sql = """
+                INSERT INTO articles (title, body, user_id) VALUES ('%s', '%s', %d)
+            """ % (title, body, user_id)
+            cur.execute(sql)
+            conn.commit()  # commit to make sure changes are saved
+            cur.close()
+            conn.close()
+            # an example with redirect
+            print("success")
+            return jsonify({'status': 200, 'message': 'Success', 'redirect': '/'})
+
+        # else will be error
+        print("error")
+        return jsonify({'status': 500, 'message': 'No Data submitted'})
+
+    return render_template('create.html')
+ 
