1c50e88970a060a296eb80fbbdfecf445e4e63c7
basementdwellers69 <tech.blade7@gmail.com>
2022-04-26 01:00:13 +0700

    added empty case on course

--- database/db.py
+++ database/db.py
@@ -3,6 +3,6 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 engine = create_engine('mysql+mysqldb://root:@localhost/pdt')
 metadata = MetaData()
-db_session = scoped_session(sessionmaker(autocommit=True, autoflush=False, bind=engine))
+db_session = scoped_session(sessionmaker(autocommit=True, autoflush=True, bind=engine))
 def init_db():
     metadata.create_all(bind=engine)
--- views/course.py
+++ views/course.py
@@ -10,16 +10,29 @@ def construct_course_bp(user, clas, content, enroll):
             return redirect(url_for('index.index'))
 
         courseData = clas.get("id='%s'" % (id)).fetchone()
-        res = "Course Data : " + str({k:v for (k,v) in courseData.items()}) + "<br>"
+        print(courseData)
+        if not courseData :
+            res = "Course Data is not Found"
+        else :
+            res = "Course Data : " + str({k:v for (k,v) in courseData.items()}) + "<br>"
 
         lecturerData = user.get("id='%s'" % (courseData["lecturer"])).fetchone()
-        res += "Lecturer Data : " + str({k:v for (k,v) in lecturerData.items()}) + "<br>"
+        if not lecturerData :
+            res += "Lecturer Data is not Found <br>"
+        else :
+            res += "Lecturer Data : " + str({k:v for (k,v) in lecturerData.items()}) + "<br>"
         
         courseContent = content.get("courseId='%s'" % (id)).fetchall()
-        res += "Course Content : " + str(["<br>"+str({k:v for (k,v) in x.items()})+"<br>" for x in courseContent]) + "<br>"
+        if not courseContent :
+            res += "Course Content Data is not Found <br>"
+        else :
+            res += "Course Content : " + str(["<br>"+str({k:v for (k,v) in x.items()})+"<br>" for x in courseContent]) + "<br>"
 
         enrollList = enroll.raw("SELECT users.id, users.status, users.firstName, users.lastName, users.email FROM users INNER JOIN course_enrolls ON course_enrolls.userId = users.id").fetchall()
-        res += "Enrolled Student : " + str(["<br>"+str({k:v for (k,v) in x.items()})+"<br>" for x in enrollList]) + "<br>"
+        if not enrollList :
+            res += "enroll Data is not Found <br>"
+        else :
+            res += "Enrolled Student : " + str(["<br>"+str({k:v for (k,v) in x.items()})+"<br>" for x in enrollList]) + "<br>"
 
         return(res)
 
