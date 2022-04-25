e91a61e0231a1944c71eaddcaf391c25130b3e1d
basementdwellers69 <tech.blade7@gmail.com>
2022-04-25 15:12:28 +0700

    profile join operation example

--- views/profile.py
+++ views/profile.py
@@ -6,6 +6,9 @@ def construct_profile_bp(controller):
 
     @profile_bp.route("/profile")
     def profile():
+        query = "SELECT users.id, users.status, users.firstName, users.lastName, users.email, majors.majorName FROM `users` INNER JOIN majors ON majors.id=users.majorId;"
+        stmp = controller.raw(query)
+        stmp.fetchall()
         return render_template('profile/profile.html')
 
     
