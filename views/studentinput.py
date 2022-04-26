b4e6bada89406af2fb08784831b7ece32beaa143
zxcvbnm0091 <91945841+zxcvbnm0091@users.noreply.github.com>
2022-04-26 23:05:16 +0700

    update studentinput.py

--- views/studentinput.py
+++ views/studentinput.py
@@ -7,33 +7,33 @@ def construct_studentinput_bp(controller):
 
     @studentinput_bp.route('/studentinput', methods=['GET', 'POST'])
     def studentinput():
-        if request.method == 'POST':
-            firstname = request.form['firstname']
-            lastname = request.form['lastname']
-            address = request.form['address']
-            country = request.form['country']
-            city = request.form['city']
-            postalcode = request.form['postalcode']
+        #if request.method == 'POST':
+        #    firstname = request.form['firstname']
+        #    lastname = request.form['lastname']
+        #    address = request.form['address']
+        #    country = request.form['country']
+        #    city = request.form['city']
+        #    postalcode = request.form['postalcode']
 
-            username = request.form['username']
-            email = request.form['email']
-            password = request.form['password']
-            status = request.form['status']
-            major = request.form['major']
+        #    username = request.form['username']
+        #    email = request.form['email']
+        #    password = request.form['password']
+        #    status = request.form['status']
+        #    major = request.form['major']
 
             #check if account exists
-            query = "username = '%s'" % (username)
-            stmp = controller.get(query)
-            user = stmp.fetchone()
-            print(user)
+        #    query = "username = '%s'" % (username)
+        #    stmp = controller.get(query)
+        #    user = stmp.fetchone()
+        #    print(user)
 
-            if user:
-                flash('Account already exists!')
-            else:
+        #    if user:
+        #        flash('Account already exists!')
+        #    else:
             #account doesn't exists insert to database
-                input = controller.raw("INSERT INTO users (firstName, lastName, address, country, city, postalCode, username, email, password, status, majorId) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', %s, %s)")
+        #        input = controller.raw("INSERT INTO users (firstName, lastName, address, country, city, postalCode, username, email, password, status, majorId) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', %s, %s)")
                 
-                flash('You have successfully registered!')
+        #        flash('You have successfully registered!')
                 
         return render_template('studentinput/studentinput.html')
 
