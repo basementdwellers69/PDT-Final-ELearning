983eeab40a3a6464d605216780c94890b819c244
zxcvbnm0091 <91945841+zxcvbnm0091@users.noreply.github.com>
2022-04-26 14:56:01 +0700

    Added studentInput.html

--- views/student.py
+++ views/student.py
@@ -6,7 +6,7 @@ def construct_student_bp(controller):
 
     @student_bp.route("/student")
     def student():
-        studentList = controller.raw("SELECT users.id, users.status, users.firstName, users.lastName, users.email, majors.majorName FROM users LEFT JOIN majors ON majorId = majors.id ORDER BY users.id").fetchall()
+        studentList = controller.raw("SELECT users.id, users.status, users.firstName, users.lastName, users.email, majors.majorName FROM users LEFT JOIN majors ON majorId = majors.id WHERE users.status = 3 ORDER BY users.id").fetchall()
 
         return render_template('student/student.html', stud=studentList)
     return student_bp
