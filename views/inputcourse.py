a3e2cf570def4e701691162246da5bd07ed9e693
GeraldSutarso <92673939+GeraldSutarso@users.noreply.github.com>
2022-04-26 11:43:44 +0700

    upd

---
+++ views/inputcourse.py
@@ -0,0 +1,12 @@
+	from flask import Flask, redirect, jsonify, render_template, url_for, session, request, flash, Blueprint
+import sys
+
+def construct_inputcourse_bp(student):
+    inputcourse_bp = Blueprint('inputcourse', __name__,static_folder='static',template_folder='templates')
+
+    @inputcourse_bp.route("/course/input")
+   
+#
+#
+        return render_template('inputcourse/inputcourse.html')
+    return inputcourse_bp
